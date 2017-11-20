06fd8d15ea4fc045b0c4f1ef0906c23b7d1a7f08
andy <andy@ashall.co>
2017-11-20 15:45:15 +0000

    clean up and added random array of 10000 ints

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from random import random
 
 from bubble import bubble
 from selection import selection
-from quicksort import quicksort
 from insertion import insertion
 from shell import shell
 from merge import merge
+from quicksort import quicksort
 
 l = [int(1000*random()) for i in range(10000)]
 
