4022083af50bef41ca43562936468a7b7338d071
andy <andy@ashall.co>
2017-11-21 11:24:19 +0000

    go bubble completed

--- main.go
+++ main.go
@@ -5,8 +5,23 @@ import (
   "math/rand"
 )
 
+func bubble(list []int) []int {
+  swaped := true
+  for swaped {
+    swaped = false
+    for i := 0; i < len(list) -1; i++ {
+      if list[i] > list[i + 1] {
+        list[i], list[i+1] = list[i+1], list[i]
+        swaped = true
+      }
+    }
+  }
+  return list
+}
+
 func main() {
   var list []int
   for i := 0; i < 1000; i++ {list = append(list, rand.Intn(100))}
-  fmt.Println(list)
+  sorted := bubble(list)
+  fmt.Println(sorted)
 }
