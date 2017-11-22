6513c4234ce8279094b96326544a8c63eac7ec6e
andy <andy@ashall.co>
2017-11-22 10:46:49 +0000

    haskell bubble

--- main.hs
+++ main.hs
@@ -8,17 +8,18 @@ main = do
   putStrLn $ show (bubble ran)
 
 bubble :: [Int] -> [Int]
-bubble l = bubbleSort l (length l) 0
+bubble l = bubbleSort l 0 (length l)
 
 bubbleSort :: [Int] -> Int -> Int -> [Int]
 bubbleSort l n e
-  | n == length l = bubbleSort l 0 (e-1)
-  | e == 0 = l
-  | otherwise = if l!!n > l!!(n+1)
-        then 
-          let i = l !! n
-              j = l !! (n+1)
-              left = take n l
-              right = drop (n+1) l
-          in bubbleSort (left ++ [j] ++ [i] ++ right) (n+1) e
-        else bubbleSort l (n+1) e
+  | n >= (length l) -1 = bubbleSort l 0 (e-1)
+  | e <= 0 = l
+  | otherwise = 
+    if l!!n > l!!(n+1)
+    then 
+      let i = l !! n
+          j = l !! (n+1)
+          left = take (n-1) l
+          right = drop (n+1) l
+      in bubbleSort (left ++ [j] ++ [i] ++ right) (n+1) e
+    else bubbleSort l (n+1) e
