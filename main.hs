c6c364b9074c3623efb8aa30642c2ab71b1c1269
andy <andy@ashall.co>
2017-11-24 12:55:49 +0000

    haskell shell

--- main.hs
+++ main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import System.Random.Shuffle
-import Data.List (delete, insert)
+import Data.List (delete, insert, transpose, insert, unfoldr)
 
 main :: IO () 
 main = do
   ran <- shuffleM [1..1000] 
-  putStrLn $ show (quickSort ran)
+  putStrLn $ show (shellSort ran)
 
 bubble :: [Int] -> [Int]
 bubble l = bubbleSort l 0 ((length l) -1)
@@ -31,6 +31,20 @@ selectionSort l = m : (selectionSort e)
                   where m = minimum l
                         e = delete m l
 
+shellSort :: (Ord a) => [a] -> [a]
+shellSort xs = foldr shellSortPhase xs gaps
+  where gaps = takeWhile (< length xs) sedgewick
+        sedgewick = concat [[9 * 2^n - 9 * 2^(n `div` 2) + 1, 8 * 2^(n+1) - 6 * 2^(n `div` 2) + 1] | n <- [0..]]
+
+shellSortPhase :: (Ord a) => Int -> [a] -> [a]
+shellSortPhase l = decolumnize . map insertionSort . columnize l
+
+columnize :: Int -> [a] -> [[a]]
+columnize l = transpose . takeWhile (not . null) . unfoldr (Just . splitAt l)
+
+decolumnize :: [[a]] -> [a]
+decolumnize = concat . transpose
+
 insertionSort :: (Ord a) => [a] -> [a]
 insertionSort = foldr insert []
 
@@ -41,6 +55,6 @@ mergeSort xs     = xs
 quickSort :: (Ord a) => [a] -> [a]  
 quickSort [] = []  
 quickSort (x:xs) =   
-    let smallerSorted = quicksort [a | a <- xs, a <= x]  
-        biggerSorted = quicksort [a | a <- xs, a > x]  
+    let smallerSorted = quickSort [a | a <- xs, a <= x]  
+        biggerSorted = quickSort [a | a <- xs, a > x]  
     in  smallerSorted ++ [x] ++ biggerSorted  
